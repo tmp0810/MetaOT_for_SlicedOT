759c44cdac259986d17d80b9536719a966ee3c75
tmp0810 <truongminhphuc08102005@gmail.com>
2026-03-10 17:45:51 +0700

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -17,11 +17,11 @@ def init_cfg(n_solver):
         cfg_m.insert("MLP_hidden_num", 3)
 
     elif n_solver == "OT_Regression_Sliced":
-        cfg_m.insert("num_bootstrap", 50)
+        cfg_m.insert("num_bootstrap", 10)
         cfg_m.insert("num_projections", 100)
         cfg_m.insert("ridge", 1e-3)
         cfg_m.insert("img_size", 28)
-        cfg_m.insert("epsilon", 0.005)
+        cfg_m.insert("epsilon", 0.1)
         cfg_m.insert("batch_size", 64)
         cfg_m.insert("valid_rate", 0.0)
         cfg_m.insert("log_interval", 1)
