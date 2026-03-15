602b29aa590b5d799e6b3084d78b6b938193ed6e
tmp0810 <truongminhphuc08102005@gmail.com>
2026-03-15 11:34:01 +0700

    Update OT_Regression_Sliced.py

--- Solvers/OT_Regression_Sliced.py
+++ Solvers/OT_Regression_Sliced.py
@@ -39,30 +39,6 @@ class OT_Regression_Sliced(Defense_Train_Base):
         diff = self.x_grid[:, None, :] - self.x_grid[None, :, :]
         self.C = np.sum(diff ** 2, axis=-1)                      
 
-    # def _solve_entropic_ot(self, a: np.ndarray, b: np.ndarray):
-    #     eps = self.cfg_m.epsilon
-
-    #     a_safe = np.clip(a, 1e-10, None)
-    #     a_safe /= a_safe.sum()
-    #     b_safe = np.clip(b, 1e-10, None)
-    #     b_safe /= b_safe.sum()
-
-    #     log_a = np.log(a_safe)   
-    #     log_b = np.log(b_safe)   
-    #     log_K = -self.C / eps    
-
-    #     def lse(X, axis):
-    #         m = X.max(axis=axis, keepdims=True)
-    #         return np.log(np.exp(X - m).sum(axis=axis)) + m.squeeze(axis=axis)
-        
-    #     f = np.zeros_like(a_safe)
-
-    #     for _ in range(self.cfg_m.sinkhorn_iters):
-    #         g = eps * (log_b - lse(log_K + f[:, None] / eps, axis=0))
-    #         f = eps * (log_a - lse(log_K + g[None, :] / eps, axis=1))
-
-    #     return f, g
-
     def _solve_entropic_ot(
         self,
         a: np.ndarray,
@@ -72,28 +48,33 @@ class OT_Regression_Sliced(Defense_Train_Base):
         import ot 
         
         eps = self.cfg_m.epsilon
- 
-        P_gt = ot.sinkhorn(a, b, self.C, reg=eps, numItermax=self.cfg_m.sinkhorn_iters, stopThr=1e-6, log=False)
- 
-        a_safe = np.clip(a, 1e-10, None)
-        b_safe = np.clip(b, 1e-10, None)
-        
 
-        u = P_gt.sum(axis=1) / a_safe 
-        v = P_gt.sum(axis=0) / b_safe
+        a_safe = np.clip(a, 1e-10, None)
+        a_safe /= a_safe.sum()
+        b_safe = np.clip(b, 1e-10, None)
+        b_safe /= b_safe.sum()
+ 
+        _, log_dict = ot.sinkhorn(
+            a_safe, b_safe, self.C, 
+            reg=eps, 
+            numItermax=self.cfg_m.sinkhorn_iters, 
+            stopThr=1e-5, 
+            log=True
+        )
         
-        _, log_dict = ot.sinkhorn(a, b, self.C, reg=eps, numItermax=self.cfg_m.sinkhorn_iters, stopThr=1e-6, log=True)
-        u_opt = log_dict['u']
-        v_opt = log_dict['v']
-        
-        f = eps * np.log(np.clip(u_opt, 1e-300, None))
-        g = eps * np.log(np.clip(v_opt, 1e-300, None))
+        if 'alpha' in log_dict:
+            f = log_dict['alpha']
+            g = log_dict['beta']
+        elif 'log_u' in log_dict:
+            f = eps * log_dict['log_u']
+            g = eps * log_dict['log_v']
+        else:
+            u_opt = log_dict.get('u', np.ones_like(a))
+            v_opt = log_dict.get('v', np.ones_like(b))
+            f = eps * np.log(np.clip(u_opt, 1e-50, None))
+            g = eps * np.log(np.clip(v_opt, 1e-50, None))
 
         return f, g
-
-    
- 
-
 
     def _compute_features(self, a: np.ndarray, b: np.ndarray):
         device = torch.device(
