d163ee3f0a97ce572ceefb0b7bcbd151bbf40c9b
tmp0810 <truongminhphuc08102005@gmail.com>
2026-03-20 14:05:35 +0700

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -142,6 +142,23 @@ def init_cfg(n_solver):
         cfg_m.insert("sinkhorn_iters",  500)    # for sanity check only
         cfg_m.insert("device", "cuda")
         cfg_m.insert("gpu", 0)
+
+    elif n_solver == "OT_Objective_Sliced_Color":
+        cfg_m.insert("num_bootstrap",    50)    
+        cfg_m.insert("num_train_iter", 5000)  
+        cfg_m.insert("num_projections", 100)    
+        cfg_m.insert("n_clusters",      500)    
+        cfg_m.insert("img_size",          0)    
+        cfg_m.insert("epsilon",         0.5)   
+        cfg_m.insert("learning_rate",  1e-3)
+        cfg_m.insert("max_grad_norm",   1.0)
+        cfg_m.insert("batch_size",        1)   
+        cfg_m.insert("valid_rate",       0.0)
+        cfg_m.insert("log_interval",    100)
+        cfg_m.insert("sinkhorn_iters",  800)   
+        cfg_m.insert("device", "cuda")
+        cfg_m.insert("gpu", 0)
+        
     return cfg_m
 
 
