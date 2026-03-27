73a74e5fe71714b64fbe9e59eaea9e898699b8c3
tmp0810 <truongminhphuc08102005@gmail.com>
2026-03-27 12:30:05 +0700

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -170,6 +170,20 @@ def init_cfg(n_solver):
         cfg_m.insert("log_interval",   100)
         cfg_m.insert("device", "cuda")
         cfg_m.insert("gpu", 0)
+
+    elif n_solver == "Min_STP_World":
+        cfg_m.insert("context_dim",    32)
+        cfg_m.insert("hidden_dim",    128)
+        cfg_m.insert("num_train_iter", 5000)
+        cfg_m.insert("learning_rate",  1e-3)
+        cfg_m.insert("alpha",          0.05)
+        cfg_m.insert("max_grad_norm",  1.0)
+        cfg_m.insert("log_interval",   100)
+        cfg_m.insert("n_supply",       100)
+        cfg_m.insert("n_demand",    10_000)
+        cfg_m.insert("epsilon",        0.5)
+        cfg_m.insert("device", "cuda")
+        cfg_m.insert("gpu", 0)
         
     return cfg_m
 
